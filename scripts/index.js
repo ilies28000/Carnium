45f6e5ef42a14f07455bfd1ac896d57143a353ab
IlièsBenos <iliesbenosman@gmail.com>
2019-07-02 15:40:36 +0200

    finalisation

--- scripts/index.js
+++ scripts/index.js
@@ -331,4 +331,28 @@ burger.addEventListener('click', () => {
 			window.onload = function(){
 				// Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
 				initMap();
-			};
+            };
+            
+
+// FAQ //
+
+let reponse = document.querySelectorAll('li')
+for(let i = 0; i<reponse.length;i++) {
+    reponse[i].addEventListener(
+        'click',
+        function () {
+          if(reponse[i].classList.contains('click'))
+          {
+            reponse[i].classList.remove('click')
+          }
+          else{
+          if (document.querySelectorAll('.click').length > 0)
+          {
+            console.log('hieennnnn')
+              document.querySelector('.click').classList.remove('click')
+          }
+          reponse[i].classList.add('click')
+          }
+        }
+    )
+}
